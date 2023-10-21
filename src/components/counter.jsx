fbba8d58111dc13dc064fd7a1a80020c2ee5bf32
Ogunwusi Alice <monilolalice@gmail.com>
2023-10-21 15:57:34 +0100

    Implemented counter for increment, decrement, reset, and setValue via input.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./counter.css";
 
 const Counter = () => {
+  let [count, setCount] = useState(0);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    let num = Number(value);
+    if (String(num) == "NaN" || value == "") {
+      return count;
+    }
+    setCount((count = value));
+  };
+
   return (
     <div className="counter">
       <div className="container text-center g-3 mb-3">
@@ -11,22 +22,37 @@ const Counter = () => {
               type="text"
               placeholder="Set Counter Value"
               className="form-control form-control-lg"
+              onChange={handleChange}
             />
           </div>
           <div className="col-lg-4">
-            <h1>Count: 0</h1>
+            <h1>Count: {count}</h1>
           </div>
         </div>
         <div className="row justify-content-between mb-3">
-          <button type="button" className="col-5 btn btn-secondary btn-lg">
+          <button
+            type="button"
+            className="col-5 btn btn-secondary btn-lg"
+            onClick={() => {
+              setCount(--count);
+            }}
+          >
             Decrement -
           </button>
-          <button type="button" className="col-5 btn btn-primary btn-lg">
+          <button
+            type="button"
+            className="col-5 btn btn-primary btn-lg"
+            onClick={() => setCount(++count)}
+          >
             Increment +
           </button>
         </div>
         <div className="row">
-          <button type="button" className="col btn btn-danger btn-lg">
+          <button
+            type="button"
+            className="col btn btn-danger btn-lg"
+            onClick={() => setCount((count = 0))}
+          >
             Reset
           </button>
         </div>
