48c318f7eb4250d0d0f4bc44e46bda22e07d3c90
Ogunwusi Alice <monilolalice@gmail.com>
2023-10-22 23:40:21 +0100

    removed hamburger for media screen.

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -8,21 +8,7 @@ const NavBar = () => {
         <Link className="navbar-brand" to="/">
           ALICIA
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="navbar-toggler"
-          data-bs-target="button"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
+        <div className="justify-content-end" id="navbarNav">
           <div className="navbar-nav">
             <NavLink className="nav-item nav-link active" to="/">
               home
